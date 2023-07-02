9d3714894a74133d9938fa9cda63c293c1c32571
marquezlianna <129668304+marquezlianna@users.noreply.github.com>
2023-07-02 09:58:43 +0800

    Insert a constructor and private data field

--- Fan Project.py
+++ Fan Project.py
@@ -5,3 +5,14 @@ class Fan:
     SLOW = 1
     MEDIUM = 2
     FAST = 3
+
+    # A constructor that creates a fan with the specified speed (default SLOW), radius (default 5), color (default blue), and on (default False).
+    def __init__(self, speed=SLOW, radius=5, color='blue', on=False):
+        # A private int data field named speed that specifies the speed of the fan.
+        self.__speed = speed
+        # A private bool data field named on that specifies whether the fan is on (the default is False).
+        self.__on = on
+        # A private float data field named radius that specifies the radius of the fan.
+        self.__radius = radius
+        # A private string data field named color that specifies the color of the fan.
+        self.__color = color
