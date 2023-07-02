a2de1f92483b55e6c7ac32fbc4aab067639926d4
marquezlianna <129668304+marquezlianna@users.noreply.github.com>
2023-07-02 12:59:45 +0800

    create a class and initialize the Pet object

---
+++ Pet.py
@@ -0,0 +1,8 @@
+# A class representing a pet with name, animal type, and age attributes.
+class Pet:
+
+    # Initializes the Pet object with default attribute values.
+    def __init__(self):
+        self.__name = ""
+        self.__animal_type = ""
+        self.__age = 0
